433743b8c6c4baed9c6ed0587bee549977d615d5
Nguyễn Thu Thủy (Nước mùa thu) <55609951+nguyenthuy1681999@users.noreply.github.com>
2020-06-11 10:11:11 +0700

    commit

---
+++ infor.js
@@ -0,0 +1,8 @@
+function clickChoose1(){
+    document.getElementById("rep1").style.display = "block";  
+    document.getElementById("rep2").style.display = "none";   
+}
+function clickChoose2(){
+    document.getElementById("rep1").style.display = "none";  
+    document.getElementById("rep2").style.display = "block";   
+}
